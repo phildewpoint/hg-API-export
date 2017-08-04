46e726f2613a3aecf007dcf90a436124d756a53b
Phil DeSantis <phildewpoint@gmail.com>
2017-08-04 16:01:35 -0500

    Added additional help text

--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ apiList = [
     "Goals" 
 ]
 # each of these lists reflect the columns available to recall in the API
+# TODO - encode the array level for these columns so the API can understand which level to extract (top, lvl 1, 2, etc)
 Member_Activity_List = [
     "ID",
     "FullName",
@@ -25,6 +26,8 @@ Member_Activity_List = [
     "LastActiveDate",
     "ManagerEmpId"
 ]
+# the checkIn sessions API (https://api.highground.com/#api-CheckIn-GetCheckInSessions) is a good example for nesting
+# the ID and Cycle both live in the Data array. the 'Reviewee' data lives inside the 'Reviewee' object
 CheckIn_Cycles_List = [
     "ID",
     "Name",
@@ -101,13 +104,15 @@ goBtn = [
 ]
 
 
-def first_press(button):
+def launch_api(button):
     if button == "Cancel":
         app.stop()
     else:
         # create and return file object
         file = create_file(api_name=app.getRadioButton("apiList"))
+        # get the list of columns user selected in UI
         col_list = app.getListItems(title="field_box")
+        # based on the selected radio button, call a specific API
         if app.getRadioButton(title="apiList") == "Member Activity":
             loop_api(api_key=apiKey, file=file, col_list=col_list, end_pt="MembersActivity")
         elif app.getRadioButton(title="apiList") == "CheckIn Cycles":
@@ -149,18 +154,23 @@ def changer(rb):
     app.updateListItems(title="field_box", items=val)
 
 
-# initialize and launch GUI
+# create GUI object from appJar
 app = gui()
+# API uses a header value as password. Need to collect it from users
+# TODO - require user to enter the key. If blank, popup error box.
 apiKey = app.addLabelEntry(title="API Key")
 app.addHorizontalSeparator(colour=None)
 app.addLabel(title="guiLabel", text="Which API do you want to extract?")
+# Creates 1 radio button per list item
 for i in apiList:
     app.addRadioButton(title="apiList", name=i, )
-app.addLabel(title="fields", text="")
+# creates multi-select list that defaults the first set of columns for Member Activity API
 app.addListBox(name="field_box", values=Member_Activity_List)
+# allows list box to act as multi-select
 app.setListBoxMulti(title="field_box", multi=True)
+# when radio button changes, call function 'changer' to change the columns
 app.setRadioButtonChangeFunction("apiList", changer)
 app.addLabel(title="dirLabel", text="You'll select the directory to save the file after clicking submit")
-app.addButtons(names=goBtn, funcs=first_press)
+app.addButtons(names=goBtn, funcs=launch_api)
 app.go()
 
